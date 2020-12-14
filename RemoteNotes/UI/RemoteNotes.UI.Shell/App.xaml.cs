e9b15c370602e1dc11f07fa23351d9915fd7c437
Mark Iliev <ilievmarik@gmail.com>
2020-12-14 08:22:50 +0200

    Changed target version for android project

--- RemoteNotes/UI/RemoteNotes.UI.Shell/App.xaml.cs
+++ RemoteNotes/UI/RemoteNotes.UI.Shell/App.xaml.cs
@@ -1,27 +1,52 @@
-﻿namespace RemoteNotes.UI.Shell
+﻿using Autofac;
+using RemoteNotes.UI.Shell.Module;
+using RemoteNotes.UI.Shell.Navigation;
+using RemoteNotes.UI.ViewModel;
+using RemoteNotes.UI.ViewModel.Service;
+using Xamarin.Forms;
+
+namespace RemoteNotes.UI.Shell
 {
     public partial class App
     {
+        private static IContainer Container;
+
+        internal static T Resolve<T>() => Container.Resolve<T>();
+        
+        internal static T Resolve<T>(string key) => Container.ResolveNamed<T>(key);
+            
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new NavigationPage();
+            RegisterDependencies();
+            SetupNavigation();
         }
 
         protected override void OnStart()
         {
-            // Handle when your app starts
         }
 
         protected override void OnSleep()
         {
-            // Handle when your app sleeps
         }
 
         protected override void OnResume()
         {
-            // Handle when your app resumes
+        }
+
+        private void RegisterDependencies()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<MainModule>();
+            builder.Register(c => new NavigationProvider(() => MainPage.Navigation));
+            Container = builder.Build();
+        }
+
+        private async void SetupNavigation()
+        {
+            var navController = Container.Resolve<INavigationController>();
+            await navController.NavigateToAsync(PageKeys.Login);
         }
     }
 }
