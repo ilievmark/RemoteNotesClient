d1f54fa6b8f5598b279a785502fc12ca2e3a34dc
Mark Iliev <ilievmarik@gmail.com>
2021-12-12 23:02:23 +0200

    Added view model navigation behavior interfaces

---
+++ RemoteNotes.Client/RemoteNotes/Domain/RemoteNotes.Domain.Contract/ViewModel/INavigated.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using RemoteNotes.Domain.Core.Navigation;
+
+namespace RemoteNotes.Domain.Contract.ViewModel
+{
+    public interface INavigated
+    {
+        Task OnNavigatedAsync(NavigationData navigationData, CancellationToken token);
+    }
+}
---
+++ RemoteNotes.Client/RemoteNotes/Domain/RemoteNotes.Domain.Contract/ViewModel/INavigatedBack.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using RemoteNotes.Domain.Core.Navigation;
+
+namespace RemoteNotes.Domain.Contract.ViewModel
+{
+    public interface INavigatedBack
+    {
+        Task OnNavigatedBackAsync(NavigationData navigationData, CancellationToken token);
+    }
+}
