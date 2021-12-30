06732bf46334b792a93d08c49c0be6e5d3a93c6f
Mark Iliev <ilievmarik@gmail.com>
2021-12-30 13:20:19 +0200

    Added sign up page

---
+++ RemoteNotes.Client/RemoteNotes/UI/RemoteNotes.UI.ViewModel/Authorization/SignUpViewModel.cs
@@ -0,0 +1,101 @@
+using System;
+using System.IO;
+using System.Security.Authentication;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using RemoteNotes.Domain.Contract.Authorization;
+using RemoteNotes.Domain.Contract.Navigation;
+using RemoteNotes.Domain.Core.Attributes;
+using RemoteNotes.Domain.Core.Constants;
+using RemoteNotes.Domain.Extensions;
+using RemoteNotes.Domain.Services.ViewModel;
+using RemoteNotes.Service.Client.Contract.Authorization;
+using RemoteNotes.UI.ViewModel.Tool;
+
+namespace RemoteNotes.UI.ViewModel.Authorization
+{
+    [ViewModelRegistration(NavigationTag = PageTagConstants.SignUp)]
+    public class SignUpViewModel : BaseNavigationViewModel
+    {
+        private readonly IAuthorizationHolder _authorizationHolder;
+        private readonly IAuthorizationService _authorizationService;
+        
+        public SignUpViewModel(
+            INavigationService navigationService,
+            IUserDialogs userDialogs,
+            IAuthorizationHolder authorizationHolder,
+            IAuthorizationService authorizationService)
+            : base(navigationService, userDialogs)
+        {
+            _authorizationHolder = authorizationHolder;
+            _authorizationService = authorizationService;
+            
+            Title = "Sign up";
+        }
+        
+        private string _login;
+        public string Login
+        {
+            get => _login;
+            set => SetProperty(ref _login, value);
+        }
+        
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+        
+        private string _confirmPassword;
+        public string ConfirmPassword
+        {
+            get => _confirmPassword;
+            set => SetProperty(ref _confirmPassword, value);
+        }
+        
+        public ICommand SignUpCommand => new AsyncCommand(() => CommandExecutionHolderMethodAsync(OnSignUpCommand));
+
+        private async Task OnSignUpCommand()
+        {
+            var login = Login;
+            var password = Password;
+            var confirmPassword = ConfirmPassword;
+
+            try
+            {
+                await SignUpAsync(login, password, confirmPassword);
+            }
+            catch (AuthenticationException authenticationException)
+            {
+                ShowToast("Sign up error. Check, you have user account");
+            }
+            catch (InvalidDataException invalidDataException)
+            {
+                ShowToast("Input data not matched the rules");
+            }
+            catch (ArgumentNullException argumentNullException)
+            {
+                ShowToast("Credentials cant be empty");
+            }
+        }
+
+        private async Task SignUpAsync(string login, string password, string confirmPassword)
+        {
+            if (!password.Equals(confirmPassword))
+            {
+                ShowToast("Passwords should be the same");
+                return;
+            }
+            
+            await _authorizationService.SignInAsync(login, password, CancellationToken.None);
+
+            if (_authorizationHolder.GetLastSession().IsValid())
+                await NavigationService.NavigateWithReplaceAsync(PageTagConstants.Dashboard, CancellationToken.None);
+            else
+                await ShowAlertAsync("Cant login with given credentials");
+        }
+    }
+}
