36b101b9c4c56364f326dec8c465e74736abbcf3
Mark Iliev <ilievmarik@gmail.com>
2022-01-07 17:30:27 +0200

    Updated auth stubs

--- RemoteNotes.Client/RemoteNotes/BL/RemoteNotes.API.Stub/AuthorizationApi.cs
+++ RemoteNotes.Client/RemoteNotes/BL/RemoteNotes.API.Stub/AuthorizationApi.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Security.Authentication;
 using System.Threading.Tasks;
 using RemoteNotes.API.Contract;
 using RemoteNotes.Domain;
+using RemoteNotes.Domain.Exceptions.Authorization;
 using RemoteNotes.Domain.Models;
 using RemoteNotes.Domain.Requests;
 using RemoteNotes.Domain.Response;
@@ -10,9 +12,26 @@ namespace RemoteNotes.API.Stub
 {
     public class AuthorizationApi : IAuthorizationApi
     {
+        private SignInRequest _acceptableRequest;
+        
+        public AuthorizationApi()
+        {
+            _acceptableRequest = new SignInRequest
+            {
+                Username = "JonieJi29",
+                Password = "12345t67890"
+            };
+        }
+        
         public async Task<ApiResponse<AuthorizationResponse>> SignInAsync(SignInRequest request)
         {
             await Task.Delay(2000);
+            
+            if (_acceptableRequest.Username != request.Username)
+                throw new AuthenticationException();
+            
+            if (_acceptableRequest.Password != request.Password)
+                throw new AuthenticationException();
             
             var response = new ApiResponse<AuthorizationResponse>();
             var token = new TokenModel
@@ -29,6 +48,9 @@ namespace RemoteNotes.API.Stub
         {
             await Task.Delay(2000);
             
+            if (_acceptableRequest.Username == request.Username)
+                throw new AuthorizationException();
+
             var response = new ApiResponse<AuthorizationResponse>();
             var token = new TokenModel
             {
