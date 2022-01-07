f8af87df41a7a394b508589ddf3ca183b17acf76
Mark Iliev <ilievmarik@gmail.com>
2022-01-07 14:47:53 +0200

    Added notes view page

---
+++ RemoteNotes.Client/RemoteNotes/UI/RemoteNotes.UI.ViewModel/Notes/ViewNoteViewModel.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using Plugin.Media.Abstractions;
+using RemoteNotes.Domain.Contract.Navigation;
+using RemoteNotes.Domain.Contract.ViewModel;
+using RemoteNotes.Domain.Core.Attributes;
+using RemoteNotes.Domain.Core.Constants;
+using RemoteNotes.Domain.Core.Extensions;
+using RemoteNotes.Domain.Core.Navigation;
+using RemoteNotes.Domain.Models;
+using RemoteNotes.UI.ViewModel.Tool;
+
+namespace RemoteNotes.UI.ViewModel.Notes
+{
+    [ViewModelRegistration(NavigationTag = PageTags.ViewNote)]
+    public class ViewNoteViewModel : NoteEditViewModelBase, INavigating
+    {
+        public ViewNoteViewModel(
+            INavigationService navigationService,
+            IUserDialogs userDialogs,
+            IMedia mediaService)
+            : base(navigationService, userDialogs, mediaService)
+        {
+        }
+        
+        public ICommand EditCommand => new AsyncCommand(() => OnEditCommandAsync(Note.Note));
+
+        protected override Task OnSaveAsync(NoteModel note)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnNavigatingAsync(NavigationData navigationData, CancellationToken token)
+        {
+            var note = navigationData.GetParameter<NoteViewModel>("Note");
+            Note = note;
+            return Task.CompletedTask;
+        }
+
+        private Task OnEditCommandAsync(NoteModel noteNote)
+        {
+            return NavigationService.NavigateNextAsync(PageTags.EditNote, CancellationToken.None,
+                new KeyValuePair<string, object>("Note", Note.Note));
+        }
+    }
+}
